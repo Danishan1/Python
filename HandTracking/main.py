5324f2c88e6b1409d60d41c368764e5d4ba1e4f0
Danishan <danishan089@gmail.com>
2023-10-23 21:06:43 +0530

    Hand Tracking Module Completed

--- HandTracking/main.py
+++ HandTracking/main.py
@@ -1,26 +1,31 @@
 import cv2
 import mediapipe as mp
 import time
-import handTracking_module as htm
+import HandTrackingModule as htm
 
 
 pTime = 0
 cTime = 0
 cap = cv2.VideoCapture(0)
-detector = htm.handDetector()
+detector = htm.HandTracker()
+skyBlue = (235, 206, 135)
+
 while True:
     success, img = cap.read()
-    img = detector.findHands(img, draw=True)
-    lmList = detector.findPosition(img, draw=False)
-    if len(lmList) != 0:
-        print(lmList[4])
+    detector.findHand(img)
+    lmList = detector.findlmk()
+    
+    detector.drawSkl(img, lmList)
+    
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (10, 70),
-                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.putText(img, str(int(fps)), (10, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, skyBlue, 2)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    
+    if cv2.waitKey(1) & 0xFF == 27:
+            break
